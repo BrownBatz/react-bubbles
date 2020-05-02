38c45bfab5161a7e334a4a1b808e51f6069c93f1
brownbatz <gabelopez@Claires-MacBook-Pro.local>
2020-05-01 20:50:38 -0600

    I have hit MVP for this project

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Login = () => {
+import {axiosWithAuth} from "./axiosWithAuth";
+
+import axios from "axios";
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({"username": "", "password": ""});
+
+  const login = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem('token', res.data.payload);
+        props.history.push("/bubblepage");
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setCredentials({...credentials, [e.target.name]: e.target.value});
+  }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+          />
+        <input
+          type="text"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+          />
+        <button>Login</button>
+      </form>
     </>
   );
 };
---
+++ client/src/components/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export function axiosWithAuth(){
+    const token = localStorage.getItem("token");
+
+    return (
+        axios.create({
+            headers: {
+                Authorization: token
+            },
+        })
+    );
+}
